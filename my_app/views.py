fa7eaed124dd6985ecbc9434ec024c41213bf13f
JamesBumanlag <jamesbumanlag@gmail.com>
2024-06-11 00:01:29 +1000

    models.py

--- my_app/views.py
+++ my_app/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from .forms import SignUpForm
 from .models import Record
 
-
 def home(request):
     return render(request, 'home.html')
 
@@ -23,7 +22,6 @@ def reset(request):
 
 
 def navbar(request):
-    # User login 
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -50,38 +48,60 @@ def logout_user(request):
 
 
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # Authenticate and login
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
+    
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+        
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+           
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+            
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, f"You Have Successfully Registered! Welcome! {username}")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'login/register.html', {'form':form})
 
-            user = authenticate(request, username = username, password = password)
-            login(request, user)
-
-            messages.success(request, "Registered Successfully")
-            return redirect('residents')
-    else:
-        form = SignUpForm()
-        return render(request, 'login/register.html', {'form':form})
+	return render(request, 'login/register.html', {'form':form})
 
 
-    return render(request, 'login/register.html', {'form':form})
+    
 
 def residents(request):
-    records = Record.objects.all()
-    return render(request, 'login/residents.html', {'records':records})
+    if request.user.is_authenticated:
+        # Look up records
+        records = Record.objects.all()
+        return render(request, 'residents.html',{'records':records})
+    else:
+         messages.success(request, 'You must be logged in')
+         return redirect('home')
+
+
+
+
+
+    # # Check to see if logging in
+    # if request.method == 'POST':
+    #      username = request.POST['username']
+    #      password = request.POST['password']
+
+    #      # Authenticate
+    #      user = authenticate(request, username=username, password=password)
+    #      if user is not None:
+    #           login(request, user)
+    #           messages.success(request, "You have been logged in")
+    #           return redirect('residents')
+    #      else:
+    #           messages.success(request, 'Error Logging In')
+    #           return redirect('home')
+    # else:
+         
+    #      return render(request, 'login/residents.html',{'records':records})
 
 def add_res(request):
     return render(request, 'login/add_res.html')
-
-def record(request, pk):
-    if request.user.is_authenticated:
-        # look up record
-        res_record = Record.objects.get(id=pk)
-        return render(request, 'login/record.html',{'res_record':res_record})
-    else:
-        messages.success(request, "You Must Be Logged In")
-        return redirect('home')
